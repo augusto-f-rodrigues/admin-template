3ce4f89bbbd2a72258a0e9b18810866a3529ea3a
Augusto Rodrigues <augusto.rodrigues@growattbrasil.com.br>
2022-09-22 14:04:06 -0300

    Aprendendo a context api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import Layout from "../components/templates/Layout";
+import useAppData from "../data/hook/useAppData";
 
 export default function Home() {
+  const data = useAppData();
   return (
     <Layout titulo="Titulo" subtitulo="Subtitulo">
       <h3>Conteúdo</h3>
+      <h3>{data.nome}</h3>
     </Layout>
   );
 }
